19eddefc6a8a9cebfdbd123ef55ad434d4fdb94b
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2024-03-25 10:27:30 +0700

    filter

--- routes/books.js
+++ routes/books.js
@@ -2,31 +2,57 @@ const e = require('express');
 var express = require('express');
 var router = express.Router();
 var bookModel = require('../schemas/book')
-var responseHandle = require('../helpers/responseHandle')
-/*
-Sửa lại code của hàm get sao cho
-Ngoài các giá trị gồm limit, sort, page thì cacs giá trị khác sẽ được 
-tìm kiếm filter
-*/
+var responseHandle = require('../helpers/responseHandle');
+const { query } = require('express');
+
 
 router.get('/', async function (req, res, next) {
-  let limit = req.query.limit ? req.query.limit : 5;
-  let page = req.query.page ? req.query.page : 1;
-  let objSort = {};
-  if (req.query.sort) {
-    if (req.query.sort.startsWith('-')) {
-      let field = req.query.sort.substring(1, req.query.sort.length);
-      objSort[field] = -1;
-    } else {
-      let field = req.query.sort;
-      objSort[field] = 1;
+  try {
+    let excludes = ["sort", "limit", "page"];
+    let numberArray = ["year"];
+    let stringArray = ["author", "name"];
+    let queries = JSON.parse(JSON.stringify(req.query));
+    for (const [key, value] of Object.entries(queries)) {
+      if (excludes.includes(key)) {
+        delete queries[key]
+      } else {
+        if (stringArray.includes(key)) {
+          queries[key] = new RegExp(value.replace(',', '|'), 'i')
+        } else {
+          if (numberArray.includes(key)) {
+            var string = JSON.stringify(value);
+            var index = string.search(new RegExp('lte"|gte"|lt"|gt"', 'i'));
+            if (index > -1) {
+              string = string.slice(0, index) + "$" + string.slice(index, string.length);
+              queries[key] = JSON.parse(string);
+            }
+            else {
+              queries[key] = value;
+            }
+          }
+        }
+      }
     }
+    queries.isDeleted = false;
+    console.log(queries);
+    let limit = req.query.limit ? req.query.limit : 5;
+    let page = req.query.page ? req.query.page : 1;
+    let objSort = {};
+    if (req.query.sort) {
+      if (req.query.sort.startsWith('-')) {
+        let field = req.query.sort.substring(1, req.query.sort.length);
+        objSort[field] = -1;
+      } else {
+        let field = req.query.sort;
+        objSort[field] = 1;
+      }
+    }
+    var books = await bookModel.find(queries)
+      .skip((page - 1) * limit).limit(limit).sort(objSort).exec();
+    res.send(books);
+  } catch (error) {
+    responseHandle.renderResponse(res, false, error);
   }
-  var books = await bookModel.find({
-    isDeleted: false,
-    name: new RegExp(req.query.name.replace(",","|"),'i')
-  }).skip((page - 1) * limit).limit(limit).sort(objSort).exec();
-  res.send(books);
 });
 
 router.get('/:id', async function (req, res, next) {
